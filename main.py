cd50e0e3d94ef9b2ca8d8f5953117ee32290b191
RunescapeLife <29634490+RunescapeLife@users.noreply.github.com>
2025-09-13 04:33:09 -0400

    Added asteroids, player character, movement, and containers for update loop

--- main.py
+++ main.py
@@ -1,19 +1,52 @@
+import sys
 import pygame
 import constants
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    game_clock = pygame.time.Clock()
+    dt = 0
     print("Starting Asteroids!")
     print(f"Screen width: {constants.SCREEN_WIDTH}")
     print(f"Screen height: {constants.SCREEN_HEIGHT}")
-    
-    while True:
+        
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    player = Player(constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT / 2)
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable, )
+    asterioid_field = AsteroidField()
+
+    running = True
+    while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return
+                running = False
+
+        for sprite in updatable:
+            sprite.update(dt)
+        
+        for sprite in asteroids:
+            if sprite.is_colliding(player):
+                print("Game Over!")
+                sys.exit()
+
         screen.fill("black")
+
+        for sprite in drawable:
+            sprite.draw(screen)
+
         pygame.display.flip()
+
+        dt = game_clock.tick(60) / 1000
 
 
 
